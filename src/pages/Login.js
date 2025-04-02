121c3d19817553bf5a39722c4fc447ea8b3cc5a5
Bartek <bartosz.papinski00@gmail.com>
2025-04-02 15:25:44 +0200

    login page styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,42 @@
 import "./styles/style.css";
 import "./styles/reset.css";
+import "./styles/login.css";
 import Header from "../Components/Header";
 import Subheader from "../Components/Subheader";
 import Sidebar from "../Components/Sidebar";
+
+
+
 
 export default function Login() {
   return (
     <div className="container">
       <Header />
-      
-      <Subheader/>
-      
-      {/* Main content */}
-      <div className="main">
-        <h1>LOGIN PAGE</h1>
+      <Subheader />
+      <div className="main-login">
+        <div className="login-flex-item"></div>
+        <div className="login-box">
+          <h2>Logowanie</h2>
+          <form className="login-form">
+            <input type="text" placeholder="Login" className="input-field" />
+            <input type="password" placeholder="Hasło" className="input-field" />
+            <button type="submit" className="login-button">Zaloguj się</button>
+          </form>
+          <div className="social-login">
+          <button class="login-with-google-btn">
+              <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=" 
+                  alt="Google Logo" class="google-logo"/>
+              <span class="google-text">Zaloguj się przy użyciu Google</span>
+          </button>
+          <button class="login-with-apple-btn">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple Logo" class="apple-logo"/>
+            <span class="apple-text">Zaloguj się przez Apple</span>
+             
+          </button>
+          </div>
+          
+        </div>
+        <div className="login-flex-item"></div>
       </div>
     </div>
   );
