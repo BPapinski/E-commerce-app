b65e7c6da7167f91aac7cfb88311946513f93eca
Bartek <bartosz.papinski00@gmail.com>
2025-06-02 23:04:05 +0200

    login failed message

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,6 +33,8 @@ export default function Login() {
     .then((res) => {
       if (res.ok) {
         setLoggedIn(true);
+        loginError();
+        setLoginErrorMessage("");
         return res.json();
       } else {
         throw new Error("Nieprawidłowe dane logowania");
@@ -50,9 +52,20 @@ export default function Login() {
     })
     .catch((err) => {
       console.error("Błąd logowania:", err);
+      setLoginErrorMessage("Nieprawidłowe dane logowania");
+      loginError();
     });
   } 
 
+
+  const [loginErrorMessage, setLoginErrorMessage] = useState("");
+
+  function loginError(){
+    const alertBox = document.querySelector(".alert-box");
+    if (alertBox) {
+      alertBox.classList.remove("hidden");
+    }
+  }
 
   return (
     <div className="container">
@@ -79,6 +92,15 @@ export default function Login() {
               />
             <button type="submit" className="login-button">Zaloguj się</button>
           </form>
+          
+            <div className="alert-box hidden"> 
+              {loginErrorMessage && (
+                <div className="alert alert-danger">
+                  {loginErrorMessage}
+                </div>
+              )}
+            </div>
+          
           <div className="social-login">
           <button class="login-with-google-btn">
               <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=" 
